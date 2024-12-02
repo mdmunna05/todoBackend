10aeebdae8a6efb186c6a73e3b2e7086f6da2998
mdmunna05 <mdbdulmoien@gmail.com>
2024-12-02 17:30:38 +0530

    All new

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
 const app = express();
+const bodyParser = require("body-parser");
+
 app.use(bodyParser.json());
-app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Todo App Backend is Running!");
-});
+let todos = []; // An array to store the todos.
 
-
-let todos = []; // Store todos
-
-// Fetch all todos
 app.get("/todos", (req, res) => {
-    res.json(todos);
+    res.json(todos);  // Send the todo items as JSON.
 });
 
-// Add a new todo
-app.post("/todos", (req, res) => {
-    const newTodo = req.body;
-    todos.push(newTodo);
-    res.json({ message: "Todo added!", todos });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
-
-// Delete a todo by index
-app.delete("/todos/:index", (req, res) => {
-    const index = req.params.index;
-    if (index < 0 || index >= todos.length) {
-        return res.status(404).json({ message: "Todo not found!" });
-    }
-    todos.splice(index, 1);
-    res.json({ message: "Todo deleted!", todos });
-});
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
